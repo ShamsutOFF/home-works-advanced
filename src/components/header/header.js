43e4ac55f5ecc705f56b31d1df9dbb142e30bd7a
ShamsutOFF <shamsutoff@yandex.ru>
2026-01-13 08:08:47 +0300

    Вёрстка шапки

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import {DivComponent} from "../../common/div-component.js";
+import "./header.css"
 
 export class Header extends DivComponent {
     constructor(appState) {
@@ -12,6 +13,19 @@ export class Header extends DivComponent {
         this.el.innerHTML = `
             <div>
             <img src="/static/logo.svg" alt="Логотип"/>
+            </div>
+            <div class="menu">
+                <a class="menu__item" href="#">
+                    <img src="/static/search.svg" alt="Поиск иконка"/>
+                    Поиск книг
+                </a>
+                <a class="menu__item" href="#favorites">
+                    <img src="/static/favorites.svg" alt="Избранное иконка"/>
+                    Избранное
+                </a>
+                <div class="menu__counter">
+                    ${this.appState.favorites.length}
+                </div>
             </div>
         `;
         return this.el;
